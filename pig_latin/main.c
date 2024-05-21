a880b4c9ce616df63a3ed8fdcf796755d52bca88
Daniel Laurin <daniellaurin21@proton.me>
2024-05-21 01:27:49 -0400

    completed pig_latin, just needs to be tested

--- pig_latin/main.c
+++ pig_latin/main.c
@@ -8,10 +8,9 @@
  -> "igpay"). Consonant sounds can be made up of multiple consonants, such as
  the "ch" in "chair" or "st" in "stand" (e.g. "chair" -> "airchay"). X
 
- Rule 3: If
- a word starts with a consonant sound followed by "qu", move them to the end of
- the word, and then add an "ay" sound to the end of the word (e.g. "square" ->
- "aresquay").
+ Rule 3: If a word starts with a consonant sound followed by "qu", move them to
+ the end of the word, and then add an "ay" sound to the end of the word (e.g.
+ "square" -> "aresquay"). X
 
  Rule 4: If a word contains a "y" after a consonant cluster or as
  the second letter in a two letter word it makes a vowel sound (e.g. "rhythm" ->
@@ -19,6 +18,7 @@
 
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 static bool is_vowel(char c) {
 
@@ -32,41 +32,64 @@ int main(void) {
   printf("Enter a word: ");
   scanf("%s", word);
 
-  if (is_vowel(word[0])) {
+  int length = strlen(word);
 
-    printf("%s-ay", word);
+  // Rule 1:
+  if ((is_vowel(word[0])) || (word[0] == 'x' && word[1] == 'r') ||
+      (word[0] == 'y' && word[1] == 't')) {
+
+    printf("%say\n", word);
 
     return 0;
   }
 
-  if ((is_vowel(word[0]) && (word[1])) == false) {
+  // Rule 3:
+  if ((is_vowel(word[0]) == false) && (word[1] == 'q') && (word[2] == 'u')) {
 
+    pig_latin[0] = word[0];
+    char *new_word = word + 3;
+
+    printf("%s%cquay", new_word, *pig_latin);
+
+    return 0;
+  }
+
+  // Rule 2:
+  if ((is_vowel(word[0]) == false) && (is_vowel(word[1]) == false)) {
     pig_latin[0] = word[0];
     pig_latin[1] = word[1];
 
     char *new_word = word + 2;
-    printf("%s-%say\n", new_word, pig_latin);
+    printf("%s%say\n", new_word, pig_latin);
 
     return 0;
   }
-  // Add step 3 and step 4 using the is_vowel statement
 
-  else {
+  // Rule 4:
+  if ((is_vowel(word[0]) == false) && (is_vowel(word[1]) == false) &&
+      (word[2] == 'y')) {
 
     pig_latin[0] = word[0];
     char *new_word = word + 1;
-    printf("%s-%cay", new_word, *pig_latin);
+    printf("y%s%cay", new_word, *pig_latin);
+    return 0;
+  }
+
+  if ((length == 2) && (word[1] == 'y')) {
+    pig_latin[0] = word[0];
+    char *new_word = word + 1;
+
+    printf("%s%cay", new_word, *pig_latin);
+
+    return 0;
+  }
+
+  // Rule 2:
+  else {
+    pig_latin[0] = word[0];
+    char *new_word = word + 1;
+    printf("%s%cay", new_word, *pig_latin);
 
     return 0;
   }
 }
-
-//   if ((word[0] == 'a') || (word[0] == 'e') || (word[0] == 'i') ||
-//       (word[0] == 'o') || (word[0] == 'u') || (word[0] == 'y') ||
-//       (word[0] == 'x' && word[1] == 'r') ||
-//       (word[0] == 'y' && word[1] == 't')) {
-
-//     printf("%s-ay", word);
-
-//     return 0;
-//   }
