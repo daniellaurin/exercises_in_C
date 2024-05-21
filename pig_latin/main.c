4af96d86b43ffa4aadc69a9852d125156f9446a4
Daniel Laurin <daniellaurin21@proton.me>
2024-05-20 20:08:49 -0400

    cleaned up version of pig_latin

--- pig_latin/main.c
+++ pig_latin/main.c
@@ -1,4 +1,3 @@
-
 /*Rule 1: If a word begins with a vowel sound, add an "ay" sound to the end of
  the word (e.g. "apple" -> "appleay"). Please note that "xr" and "yt" at the
  beginning of a word make vowel sounds (e.g. "xray" -> "xrayay", "yttria" ->
@@ -7,7 +6,7 @@
  Rule 2: If a word begins with a consonant sound, move it to the
  end of the word and then add an "ay" sound to the end of the word (e.g. "pig"
  -> "igpay"). Consonant sounds can be made up of multiple consonants, such as
- the "ch" in "chair" or "st" in "stand" (e.g. "chair" -> "airchay").
+ the "ch" in "chair" or "st" in "stand" (e.g. "chair" -> "airchay"). X
 
  Rule 3: If
  a word starts with a consonant sound followed by "qu", move them to the end of
@@ -18,7 +17,13 @@
  the second letter in a two letter word it makes a vowel sound (e.g. "rhythm" ->
  "ythmrhay", "my" -> "ymay").*/
 
+#include <stdbool.h>
 #include <stdio.h>
+
+static bool is_vowel(char c) {
+
+  return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y';
+}
 
 int main(void) {
   char word[100];
@@ -27,18 +32,14 @@ int main(void) {
   printf("Enter a word: ");
   scanf("%s", word);
 
-  if ((word[0] == 'a') || (word[0] == 'e') || (word[0] == 'i') ||
-      (word[0] == 'o') || (word[0] == 'u') || (word[0] == 'y') ||
-      (word[0] == 'x' && word[1] == 'r') ||
-      (word[0] == 'y' && word[1] == 't')) {
+  if (is_vowel(word[0])) {
 
     printf("%s-ay", word);
 
     return 0;
   }
 
-  if ((word[0] == 's' && word[1] == 't') ||
-      (word[0] == 'c' && word[1] == 'h')) {
+  if ((is_vowel(word[0]) && (word[1])) == false) {
 
     pig_latin[0] = word[0];
     pig_latin[1] = word[1];
@@ -48,6 +49,7 @@ int main(void) {
 
     return 0;
   }
+  // Add step 3 and step 4 using the is_vowel statement
 
   else {
 
@@ -58,3 +60,13 @@ int main(void) {
     return 0;
   }
 }
+
+//   if ((word[0] == 'a') || (word[0] == 'e') || (word[0] == 'i') ||
+//       (word[0] == 'o') || (word[0] == 'u') || (word[0] == 'y') ||
+//       (word[0] == 'x' && word[1] == 'r') ||
+//       (word[0] == 'y' && word[1] == 't')) {
+
+//     printf("%s-ay", word);
+
+//     return 0;
+//   }
